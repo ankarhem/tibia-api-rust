7fe2f92efa744bce46e479d35771991f594502ef
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2023-08-28 23:34:04 +0200

    run cargo fmt

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,10 +1,10 @@
 mod game_world_type;
 mod guild;
-mod residence;
 mod kill_statistics;
 mod location;
 mod player;
 mod pvp_type;
+mod residence;
 mod transfer_type;
 mod vocation;
 mod world_details;
@@ -12,11 +12,11 @@ mod worlds;
 
 pub use game_world_type::*;
 pub use guild::*;
-pub use residence::*;
 pub use kill_statistics::*;
 pub use location::*;
 pub use player::*;
 pub use pvp_type::*;
+pub use residence::*;
 pub use transfer_type::*;
 pub use vocation::*;
 pub use world_details::*;
